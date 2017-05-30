76b54e0685888a6b216865136b383624c92eacae
Nikhila Iyer <Nikhila@Nikhilas-MacBook-Pro.local>
2017-05-29 18:49:55 -0700

    test w mtcars

--- server.R
+++ server.R
@@ -12,15 +12,18 @@ library(rgdal)
 
 
 #scripts
-source('~/Documents/College/Sophomore (2016-2017)/Spring Quarter/INFO201/twitter-weather/scripts/setup.R')
+#setwd('~/Documents/College/Sophomore (2016-2017)/Spring Quarter/INFO201/twitter-weather')
+#source('.scripts/BuildRenderedChart.R')
 
 #call buildtimeline.R
 shinyServer(function(input, output) {
-  output$mainPlot <- BuildBarPlot(twitter.data, input$time, y.var, 'Time', 'Number of Tweets', 'Number of Tweets Throughout the Day')
-  output$mainPlot <- BuildLinePlot(data, x.var, y.var, x.label, y.label, title)
+  #output$mainPlot <- BuildBarPlot(twitter.data, input$time, y.var, 'Time', 'Number of Tweets', 'Number of Tweets Throughout the Day')
+  #output$mainPlot <- BuildLinePlot(data, x.var, y.var, x.label, y.label, title)
   #output$mainPlot <- RenderPlots(plot.1, y.var.1, plot.2, y.var.2)
   
-  output$value <- renderPrint({input$dates})
-  output$value <- renderPrint({input$time})
-  output$value <- renderPrint({input$city})
+  output$mainPlot <- BuildBarPlot(mtcars, 'hp', 'drat', "MPG", "CYL", "mpg v cyl")
+  
+  # output$value <- renderPrint({input$dates})
+  # output$value <- renderPrint({input$time})
+  # output$value <- renderPrint({input$city})
 })
