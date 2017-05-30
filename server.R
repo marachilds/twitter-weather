75f52d633e16e93ad3553652abc0502c7b2bc842
Nikhila Iyer <Nikhila@Nikhilas-MacBook-Pro.local>
2017-05-29 19:11:36 -0700

    run app works

--- server.R
+++ server.R
@@ -22,7 +22,10 @@ shinyServer(function(input, output) {
   #output$mainPlot <- BuildLinePlot(data, x.var, y.var, x.label, y.label, title)
   #output$mainPlot <- RenderPlots(plot.1, y.var.1, plot.2, y.var.2)
   
-  output$mainPlot <- BuildBarPlot(mtcars, 'hp', 'drat', "MPG", "CYL", "mpg v cyl")
+  output$mainPlot <- renderPlotly({
+    return(BuildBarPlot(mtcars, 'hp', 'drat', "MPG", "CYL", "mpg v cyl"))
+
+  })
   
   output$value <- renderPrint({input$dates})
   output$value <- renderPrint({input$time})
