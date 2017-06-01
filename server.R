c859749e77e2729224306281ca1f317e6350603e
Nikhila Iyer <Nikhila@Nikhilas-MacBook-Pro.local>
2017-06-01 11:16:09 -0700

    esha's edits copied

--- server.R
+++ server.R
@@ -1,56 +1,65 @@
 # server.R
 
-#libraries
-library(anytime)
-library(shiny)
+# libraries
+## Dependencies for setup.R
 library(dplyr)
+library(anytime)
+library(jsonlite)
+library(rgeos)
+library(rgdal)
+library(rtweet) 
+
+library(shiny)
 library(plotly)
 library(httr)
-library(rgeos)
-library(jsonlite)
-library(rgdal)
-library(rtweet)
 library(stringr)
-library(rjson)
 
 #scripts
+
 source('scripts/setup.R')
 source('scripts/BuildBarChart.R')
 source('scripts/BuildLineChart.R')
 source('scripts/analysis.R')
 source('scripts/BuildRenderedChart.R')
 
-# Retrieves dataset for towns and cities in Canada/US with latitudinal and longitudinal data for API calls
-geo_data <- read.csv("geo_data.csv")
 
-## Twitter authentification credentials 
-appname <- "twitter-weather-moscow-mules"
-
-# Retrieving authentication credentials from .json 
-twitter.key <- fromJSON(file = 'access-keys.json')$twitter$consumer_key
-twitter.secret <- fromJSON(file = 'access-keys.json')$twitter$consumer_secret
-
-# create token for authentication
-twitter.token <- create_token(
-  app = appname,
-  consumer_key = twitter.key,
-  consumer_secret = twitter.secret)
+test <- weatherData("Portland", "ME", "28 May 2017")
+# twitterData(city, state, start_date, end_date)
+# test2 <- twitterData("Portland", "ME", "2017-05-25", "2017-05-26")
 
 # call buildtimeline.R
 shinyServer(function(input, output) {
   
   selectPlot <- reactive({
-  #Tweets = bar chart
-    
-      plot.1 <- BuildBarPlot(mtcars, 'mpg', 'cyl', "1", "2", "title", '13B0E9')
-      plot.2 <- BuildLinePlot(mtcars, 'hp', 'drat', "hp", "drat", "else", 'FDE600')
-      return(BuildLinePlot(mtcars, 'hp', 'drat', "hp", "drat", paste(input$chart, " hey"), 'FDE600'))
-      #return(RenderPlots(plot.1, mtcars, "mpg", plot.2, mtcars, paste(input$chart)))
-    
-})
+    #Tweets = bar chart
+    if (input$radio == 1) {
+      location <- str_split_fixed(input$city, ", ", 2)
+      #twitter.data <- twitterData(location[,1], location[,2], input$start.date, input$end.date)
+      return(BuildBarPlot(mtcars, 'mpg', 'cyl', "1", "2", "title"))
+      #return(BuildBarPlot(twitter.data, twitter.data[,time], twitter.data[,freq], "Time", "Tweets", 
+      #paste("Number of Tweets on", input$dates, "in", input$city), '13B0E9'))
+      
+    } else if (input$radio == 2) {
+      #Weather = line chart
+      location <- str_split_fixed(input$city, ", ", 2)
+      #weather.data <- weatherData(location[,1], location[,2], input$start.date)
+      return(BuildLinePlot(mtcars, 'hp', 'cyl', "3", "2", "else"))
+      #return(BuildLineChart(weather.data, 'time', "temperature", "Time", "Weather",
+      #paste("Weather on", input$dates, "in", input$city), 'FDE600'))
+    } else {
+      plot.1 <- BuildBarPlot(mtcars, 'mpg', 'cyl', "1", "2", "title")
+      plot.2 <- BuildLinePlot(mtcars, 'hp', 'cyl', "3", "2", "else")
+      return(BuildRenderPlots(plot.1, mtcars, "mpg", plot.2, mtcars, "hp", "1", "3", "Rendered"))
+    }
+  })
   
   output$fooPlot1 <- renderPlotly({
-      print(selectPlot())
+    renderPrint(selectPlot())
+    location <- strsplit(input$city, ", ")
+    weather.data <- weatherData(location[[1]][1], location[[1]][2], input$dates)
+    
+    return(BuildLineChart(weather.data, "time", "temperature",
+                          "Time", "Weather", paste("Weather on", input$dates, "in", input$city)))
   })
   
   output$value <- renderPrint({input$start.date})
